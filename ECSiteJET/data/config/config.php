3ef78e7fb1e5e4e6365c32aa69e128e2eeb15681
hishidama <>
2017-05-17 11:17:55 +0900

    Oikawa

---
+++ ECSiteJET/data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('HTTP_URL', 'http://mierusc.conf.jp/ECSiteJET/');
+define('HTTPS_URL', 'http://mierusc.conf.jp/ECSiteJET/');
+define('ROOT_URLPATH', '/ECSiteJET/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'jetest01_ectest');
+define('DB_PASSWORD', '5U6v2UC8fseF');
+define('DB_SERVER', 'mysql5.minibird.netowl.jp');
+define('DB_NAME', 'jetest01_ectest');
+define('DB_PORT', '');
+define('ADMIN_DIR', 'oikawakanri/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'triwrewiabioclibiacosumeacopupaewragiocr');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'smtp');
+define('SMTP_HOST', 'jetest01.minibird.jp');
+define('SMTP_PORT', '');
+define('SMTP_USER', 'ectest@mierusc.conf.jp');
+define('SMTP_PASSWORD', 'NC7D5APeCaRk');
